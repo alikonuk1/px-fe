63d90390d895522150c307b94e08a2c1cc01ee55
alikonuk1 <alikonuk1@gmail.com>
2023-04-01 09:12:25 +0300

    ✨

--- src/index.js
+++ src/index.js
@@ -11,13 +11,13 @@ import {
   midnightTheme
 } from '@rainbow-me/rainbowkit';
 import { configureChains, createClient, WagmiConfig } from 'wagmi';
-import { goerli, sepolia } from 'wagmi/chains';
+import { sepolia } from 'wagmi/chains';
 import { alchemyProvider } from 'wagmi/providers/alchemy';
 import { publicProvider } from 'wagmi/providers/public';
 import { ChakraProvider } from '@chakra-ui/react'
 
 const { chains, provider } = configureChains(
-  [sepolia, goerli],
+  [sepolia],
   [
     alchemyProvider({ apiKey: process.env.ALCHEMY_ID }),
     publicProvider()
